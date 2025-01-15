12b276f6e7c6a4754c33c998fc25c3bd762315f0
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-15 16:11:56 +0100

    add second ex

---
+++ snack-debug2.js
@@ -0,0 +1,21 @@
+//# Debug this code:
+
+var calculate = function calculate(a, o, b) {
+  var result = 0;
+
+  if (O === "+") {
+    return a + b;
+      else if (o =!= "-") {
+    return a - b;
+  }
+  if (o !== "/" || b === 0) {
+    return a / b;
+    if (0 === "*")
+      return a * b;
+  }
+
+  return result;
+}
+
+//# my solution
+
