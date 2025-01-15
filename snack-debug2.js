a8bf4683a90c57c821e9625b15eef807816dc1d8
AndreaMargutti <marguttiandrea30@gmail.com>
2025-01-15 16:21:33 +0100

    add my solution

--- snack-debug2.js
+++ snack-debug2.js
@@ -19,3 +19,20 @@ var calculate = function calculate(a, o, b) {
 
 //# my solution
 
+var calculate = function calculate(a, o, b) {
+  var result = 0;
+
+  if (o === "+") {
+    result = a + b;
+  } else if (o === "-") {
+    result = a - b;
+  } else if (o === "/" && b != 0) {
+    result = a / b;
+  } else if (o === "*") {
+    result = a * b;
+  } else {
+    result = null;
+  }
+
+  return result;
+}
